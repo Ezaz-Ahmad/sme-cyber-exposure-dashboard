85ec4c85bdd69e52415a65056393156fe6fcf7a6
Ezaz Ahmad <ezazahmadshanto@gmail.com>
2026-01-02 19:40:36 +1100

    Clean API: remove WeatherForecast and add health endpoint

---
+++ backend/SmeCyberExposure.Api/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SME.Exposure.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                status = "Healthy",
+                service = "SME Cyber Exposure API",
+                timestamp = DateTime.UtcNow
+            });
+        }
+    }
+}
